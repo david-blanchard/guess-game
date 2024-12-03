898179793be8b99ec28db9da28b45cd93fe0fea2
David Blanchard <dpjb@hotmail.fr>
2024-12-03 09:15:43 +0100

    Remove grpc files which are not of this project.

--- go/grpc/bin/services.ps1
+++
@@ -1,2 +0,0 @@
-cd orders
-grpcurl -d '{  "userId": "user_123",  "items": [{"productId": "product_1", "quantity": 2}, {"productId": "product_2", "quantity": 1}]}' -plaintext localhost:50052 orders.OrderService/PlaceOrder
